93c7071e46211e2c5e20838666eb4a9947810a70
teddyjoenakad <teddyjoe160@gmail.com>
2024-11-10 15:54:39 -0800

    added new logos and changed .js files

--- dashboard-ui/src/components/EndpointAnalyzer.js
+++ dashboard-ui/src/components/EndpointAnalyzer.js
@@ -8,7 +8,7 @@ export default function EndpointAnalyzer(props) {
 	const rand_val = Math.floor(Math.random() * 100); // Get a random event from the event store
 
     const getAnalyzer = () => {
-        fetch(`http://<Cloud DNS>:8110/${props.endpoint}?index=${rand_val}`)
+        fetch(`http://acit3855-kafka2.westus.cloudapp.azure.com:8110/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Analyzer Results for " + props.endpoint)
