f36972f8aab57f8afcd437bce39561c249f50ad6
teddyjoenakad <teddyjoe160@gmail.com>
2024-12-05 13:30:59 -0800

    please?

--- dashboard-ui/src/App.js
+++ dashboard-ui/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.png';
 import './App.css';
 
-import EndpointAnalyzer from './components/EndpointAnalyzer'
-import AppStats from './components/AppStats'
+import EndpointAnalyzer from './components/EndpointAnalyzer';
+import AppStats from './components/AppStats';
+import AnomaliesStats from './components/AnomaliesStats'; // Import AnomaliesStats
 
 function App() {
 
-    const endpoints = ["parking", "payment"]
+    const endpoints = ["parking", "payment"];
 
     const rendered_endpoints = endpoints.map((endpoint) => {
-        return <EndpointAnalyzer key={endpoint} endpoint={endpoint}/>
-    })
+        return <EndpointAnalyzer key={endpoint} endpoint={endpoint} />;
+    });
 
     return (
         <div className="App">
-            <img src={logo} className="App-logo" alt="logo" height="150px" width="150px"/>
+            <img src={logo} className="App-logo" alt="logo" height="150px" width="150px" />
             <div>
-                <AppStats/>
+                <AppStats />
+                <AnomaliesStats />
                 <h1>Analyzer Endpoints</h1>
                 {rendered_endpoints}
             </div>
@@ -24,7 +26,5 @@ function App() {
     );
 
 }
-
-
 
 export default App;
